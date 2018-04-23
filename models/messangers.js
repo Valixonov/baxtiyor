c19aa147b849a7846388a93cb85981795f617181
valixonov <valixonovbaxtiyor@gmail.com>
2018-04-23 18:01:24 +0500

    Baxtiyrda o'gartirilgani 23.04.2018

--- models/messangers.js
+++ models/messangers.js
@@ -1,29 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Messangers = sequelize.define('Messangers', {
-    text: DataTypes.STRING,
-    status: DataTypes.INTEGER
-  }, {});
-  Messangers.associate = function(models) {
+    var Messangers = sequelize.define('Messangers', {
+        text: DataTypes.STRING,
+        status: DataTypes.INTEGER,
+        //timedate: DataTypes.TIMESTAMP
+    }, {});
+    Messangers.associate = function (models) {
 
-    models.Messangers.belongsTo(models.Messangers, {
-      onDelete: "CASCADE",
-      foreignKey:"RetryId"
-    });
-    models.Messangers.belongsTo(models.Users, {
-      onDelete: "CASCADE",
-      foreignKey:"From_User",
-    });
-    models.Messangers.belongsTo(models.Groups, {
-      onDelete: "CASCADE",
-    });
-    models.Messangers.belongsTo(models.Users, {
-      onDelete: "CASCADE",
-      foreignKey:"To_User",
-      as:"to_User"
-    });
+        models.Messangers.belongsTo(models.Messangers, {
+            onDelete: "CASCADE",
+            foreignKey: "RetryId"
+        });
+        models.Messangers.belongsTo(models.Users, {
+            onDelete: "CASCADE",
+            foreignKey: "From_User",
+        });
+        models.Messangers.belongsTo(models.Groups, {
+            onDelete: "CASCADE",
+        });
+        models.Messangers.belongsTo(models.Users, {
+            onDelete: "CASCADE",
+            foreignKey: "To_User",
+            as: "to_User"
+        });
 
-    // associations can be defined here
-  };
-  return Messangers;
+        // associations can be defined here
+    };
+    return Messangers;
 };
